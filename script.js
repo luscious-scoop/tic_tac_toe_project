bba9844b7d44a41e22fee086d982d90c1afee6b3
Haseeb <ah085904@gmail.com>
2026-06-03 17:01:41 +0500

    game controller factory to play round

--- script.js
+++ script.js
@@ -27,14 +27,12 @@ function GameBoard() {
 		board.forEach((row) => row.forEach((cell) => cell.resetValue()));
 	};
 	return {
+		placeValues,
 		getBoard,
 		printBoard,
 		resetBoard,
 	};
 }
-
-game = GameBoard();
-console.log(game.printBoard());
 
 function Cell() {
 	let value = 0;
@@ -53,3 +51,57 @@ function Cell() {
 		resetValue,
 	};
 }
+
+function GameController(
+	playerOneName = "PlayerOne",
+	playerTwoName = "PlayerTwo",
+) {
+	let gameBoard = GameBoard();
+	let board = gameBoard.getBoard();
+	let players = [
+		{
+			name: playerOneName,
+			token: 1,
+		},
+
+		{
+			name: playerTwoName,
+			token: 2,
+		},
+	];
+
+	let activePlayer = players[0];
+
+	const switchPlayerTurn = () => {
+		activePlayer = activePlayer === players[0] ? players[1] : players[0];
+	};
+
+	const playRound = (row, column) => {
+		if (board[row][column].getValue() === 0) {
+			console.log(
+				`Placing ${activePlayer.name} token into row:${row} and column:${column}`,
+			);
+
+			gameBoard.placeValues(row, column, activePlayer.token);
+			switchPlayerTurn();
+			printRound();
+		} else {
+			alert("Spot Already taken");
+		}
+	};
+	const printRound = () => {
+		console.log(`${activePlayer.name} turn's `);
+		console.log(gameBoard.printBoard());
+	};
+
+	return {
+		playRound,
+	};
+}
+
+let game = GameController();
+game.playRound(0, 0); // player 1
+game.playRound(1, 0); // player 2
+game.playRound(0, 1); // player 1
+game.playRound(2, 0); // player 2
+game.playRound(0, 2); // player 1
